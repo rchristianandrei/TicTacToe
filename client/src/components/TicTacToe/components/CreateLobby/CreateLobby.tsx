164a5d1f76ecbca019618e22334f5e755c783b90
Andrei Reyes <rchristianandrei@gmail.com>
2025-02-16 19:53:31 +0800

    create and join lobby ui

--- client/src/components/TicTacToe/components/CreateLobby/CreateLobby.tsx
+++ client/src/components/TicTacToe/components/CreateLobby/CreateLobby.tsx
@@ -1,8 +1,36 @@
+import { useNavigate } from "react-router-dom"
+
 export default function CreateLobby(){
 
+    const navigate = useNavigate()
+
     return(
-        <main>
-            Create Lobby
+        <main className="flex h-screen justify-center items-center">
+            <div className="border-solid border-black border p-10 text-center rounded-lg">
+                <h1 className="text-6xl">Lobby #</h1>
+                <h2 className="text-4xl">123456</h2>
+                <hr className="my-3" />
+                <h3 className="text-2xl">Opponent</h3>
+                <h4 className="text-xl">None</h4>
+                <nav className="mt-5">
+                    <ul className="flex flex-col ">
+                        <li className="list-none my-2">
+                            <button onClick={() => {
+                                console.log("start")
+                            }} className="bg-blue-600 hover:bg-blue-300 border cursor-pointer p-5 rounded-lg w-50 text-[whitesmoke] hover:text-[black] text-lg">
+                                Start
+                            </button>
+                        </li>     
+                        <li className="list-none my-2">
+                            <button onClick={() => {
+                                navigate("/lobby")
+                            }} className="bg-[#FFF] hover:bg-[#BBB] border border-[#FFF] hover:border-[#000] cursor-pointer p-5 rounded-lg">
+                                Exit
+                            </button>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </main>
     )
 }
--- client/src/components/TicTacToe/components/JoinLobby/JoinLobby.tsx
+++ client/src/components/TicTacToe/components/JoinLobby/JoinLobby.tsx
@@ -1,8 +1,47 @@
+import { useRef, useState } from "react"
+import { useNavigate } from "react-router-dom"
+
 export default function JoinLobby(){
 
+    const navigate = useNavigate()
+
+    const roomNumberField = useRef<HTMLInputElement>(null)
+
+    const [errorMessage, setErrorMessage] = useState("")
+
     return(
-        <main>
-            Join Lobby
+        <main className="flex h-screen justify-center items-center">
+            <div className="border-solid border-black border p-10 text-center rounded-lg">
+                <h1 className="text-6xl my-3">Find Lobby</h1>
+                <input ref={roomNumberField} type="text" maxLength={6} className="border p-2 rounded-lg text-4xl text-center"></input>
+                {errorMessage && <div className="text-red-700">{errorMessage}</div>}
+                <nav className="mt-5">
+                    <ul className="flex flex-col ">
+                        <li className="list-none my-2">
+                            <button onClick={() => {
+                                const roomNumber = roomNumberField.current?.value
+
+                                if(!roomNumber){
+                                    setErrorMessage("Unable to join")
+                                    return
+                                }
+
+                                console.log("Join", roomNumber)
+
+                            }} className="bg-blue-600 hover:bg-blue-300 border cursor-pointer p-5 rounded-lg w-50 text-[whitesmoke] hover:text-[black] text-lg">
+                                Join
+                            </button>
+                        </li>     
+                        <li className="list-none my-2">
+                            <button onClick={() => {
+                                navigate("/lobby")
+                            }} className="bg-[#FFF] hover:bg-[#BBB] border border-[#FFF] hover:border-[#000] cursor-pointer p-5 rounded-lg">
+                                Exit
+                            </button>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </main>
     )
 }
